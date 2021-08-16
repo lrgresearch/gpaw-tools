f1e9553e933d146b306b9248b23577ed091662ea
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-08-17 01:23:57 +0300

    Solve double element problem

--- CIF-to-ASE/ciftoase.py
+++ CIF-to-ASE/ciftoase.py
@@ -6,7 +6,7 @@ import sys, os
 # Converting CIF files to ASE Atoms object to use in GPAW Input for LRG Studies
 # by Sefer Bora Lisesivdin
 # August 2021 - First version
-# Usage: $ python ConvertToASE.py
+# Usage: $ python ciftoase.py file.cif
 # -------------------------------------------------------------
 # Parameters
 # -------------------------------------------------------------
@@ -15,6 +15,9 @@ Manualpbc = False # If you need manual constraint axis
 
 # IF Manualpbc is true then change following:
 pbcmanual=[True,True,False]
+
+# If you have double number of elements in your final file
+SolveDoubleElementProblem = True
 # -------------------------------------------------------------
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\ 
 # -------------------------------------------------------------
@@ -31,16 +34,22 @@ if Scaled == True:
     positions = asestruct.get_scaled_positions()
 else:
     positions = asestruct.get_positions()
-nn=-1
-mm=-1
-for n in asestruct.get_chemical_symbols():
-    nn=nn+1
-    for m in positions:
-        mm=mm+1
-        if mm == nn:
-            if (mm % 2) != 0:
-                f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
-    mm=0
+nn=0
+mm=0
+
+if SolveDoubleElementProblem == True:
+    for n in asestruct.get_chemical_symbols():
+        nn=nn+1
+        for m in positions:
+            mm=mm+1
+            if mm == nn:
+                 f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
+        mm=0
+else:
+    for n in asestruct.get_chemical_symbols():
+        for m in positions:
+            f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
+
 f.write("    ],\n")
 f.write("    cell=[("+str(asestruct.cell[0,0])+", "+str(asestruct.cell[0,1])+", "+str(asestruct.cell[0,2])+"), ("+str(asestruct.cell[1,0])+", "+str(asestruct.cell[1,1])+", "+str(asestruct.cell[1,2])+"), ("+str(asestruct.cell[2,0])+", "+str(asestruct.cell[2,1])+", "+str(asestruct.cell[2,2])+")],\n")
 if Manualpbc == False:
@@ -59,14 +68,20 @@ else:
     positions = asestruct.get_positions()
 nn=-1
 mm=-1
-for n in asestruct.get_chemical_symbols():
-    nn=nn+1
-    for m in positions:
-        mm=mm+1
-        if mm == nn:
-            if (mm % 2) != 0:
-                print("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),")
-    mm=0
+
+if SolveDoubleElementProblem == True:
+    for n in asestruct.get_chemical_symbols():
+        nn=nn+1
+        for m in positions:
+            mm=mm+1
+            if mm == nn:
+                 print("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),")
+        mm=0
+else:
+    for n in asestruct.get_chemical_symbols():
+        for m in positions:
+            print("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),")
+
 print("    ],")
 print("    cell=[("+str(asestruct.cell[0,0])+", "+str(asestruct.cell[0,1])+", "+str(asestruct.cell[0,2])+"), ("+str(asestruct.cell[1,0])+", "+str(asestruct.cell[1,1])+", "+str(asestruct.cell[1,2])+"), ("+str(asestruct.cell[2,0])+", "+str(asestruct.cell[2,1])+", "+str(asestruct.cell[2,2])+")],")
 if Manualpbc == False:

f110c94084f3796ab309ee3291f213bce9df46a6
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-08-20 13:19:02 +0300

    Update 0-3-Optimize-KPoints.py
    Better parallel computation and its handling

--- Cutoff-Lattice-Kpoint-optimizations/0-3-Optimize-KPoints.py
+++ Cutoff-Lattice-Kpoint-optimizations/0-3-Optimize-KPoints.py
@@ -1,17 +1,19 @@
 import numpy as np
 from ase import *
 from gpaw import GPAW, PW
+from ase.parallel import paropen, world, parprint
+
 # Sample K-point Optimization GPAW Input for LRG Studies
 # by Sefer Bora Lisesivdin
+# August 2021 - Better parallel computation and its handling
 # July 2021 - Corrected version
 # March 2020 - First Version 
-# Usage: $ gpaw python script.py
+# Usage: $ gpaw -P<core_number> python Optimize_KPoints.py
 # 
 # -------------------------------------------------------------
 # ENTER PARAMETERS
 # -------------------------------------------------------------
 cutoffenergy = 200
-
 kpoint_min = 4
 kpoint_max = 11
 
@@ -31,7 +33,7 @@ bulk_configuration = Atoms(
 # CONVERGE KPOINTS
 # -------------------------------------------------------------
 cell0 = bulk_configuration.cell
-f = open('Table-K_points.txt', 'a')
+f = paropen('Optimize-KPoints_Table-KPoints.txt', 'a')
 f.write('K-point  Total_Energy\n')
 for k in range(kpoint_min, kpoint_max):
     bulk_configuration.calc = GPAW(mode=PW(cutoffenergy),
@@ -39,12 +41,8 @@ for k in range(kpoint_min, kpoint_max):
                               kpts=(k, k, k),
                               parallel={'band': 1},
                               basis='dzp',
-                              txt='Optimize-0-kpoints-%02d.txt' % k)
-    print ("K-point:"+str(k))
-#    for eps in np.linspace(-0.02, 0.02, 5):
-#        bulk_configuration.cell = (1 + eps) * cell0
-#        bulk_configuration.get_potential_energy()
-#        print ("eps:"+str(eps))
+                              txt='Optimize-KPoints_KPoints-%02d.txt' % k)
+    parprint("K-point:"+str(k)+"   Potential Energy:"+str(bulk_configuration.get_potential_energy()))
     f.write(str(k)+'  '+str(bulk_configuration.get_potential_energy())+'\n')
 f.close()
 

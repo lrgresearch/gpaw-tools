e820787419cbbd9e9e601ed7b6b7c3cb15cfdfb8
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-09-08 12:33:06 +0300

    Enhancing code quality
    Code rate has been increased from 4.44 to 6.34. Need to do more in the future.

--- CIF-to-ASE/ciftoase.py
+++ CIF-to-ASE/ciftoase.py
@@ -1,12 +1,10 @@
-from ase import Atoms
-from ase.io import read, write
-from ase.io.cif import read_cif
+'''
+ciftoase.py: Converting CIF files to ASE Atoms object to use in GPAW calculation
+Usage: $ python ciftoase.py file.cif
+'''
 import sys, os
+from ase.io import read
 
-# ciftoase.py: Converting CIF files to ASE Atoms object to use in GPAW calculation
-# by Sefer Bora Lisesivdin
-# August 2021 - First version
-# Usage: $ python ciftoase.py file.cif
 # -------------------------------------------------------------
 # Parameters
 # -------------------------------------------------------------
@@ -19,51 +17,48 @@ pbcmanual=[True,True,False]
 # If you have double number of elements in your final file
 SolveDoubleElementProblem = True
 # -------------------------------------------------------------
-# ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\ 
+# ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
 inFile = sys.argv[1]
 asestruct = read(inFile, index='-1')
 print(inFile)
 print(os.path.splitext(inFile)[0])
-f = open(os.path.splitext(inFile)[0]+'.py', 'w')
 
 # PRINT TO FILE PART -----------------------------------
-f.write("bulk_configuration = Atoms(\n")
-f.write("    [\n")
-if Scaled == True:
-    positions = asestruct.get_scaled_positions()
-else:
-    positions = asestruct.get_positions()
-nn=0
-mm=0
+with open(os.path.splitext(inFile)[0]+'.py', 'w') as f:
+    f.write("bulk_configuration = Atoms(\n")
+    f.write("    [\n")
+    if Scaled == True:
+        positions = asestruct.get_scaled_positions()
+    else:
+        positions = asestruct.get_positions()
+    nn=0
+    mm=0
 
-if SolveDoubleElementProblem == True:
-    for n in asestruct.get_chemical_symbols():
-        nn=nn+1
-        for m in positions:
-            mm=mm+1
-            if mm == nn:
-                 f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
-        mm=0
-else:
-    for n in asestruct.get_chemical_symbols():
-        for m in positions:
-            f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
-
-f.write("    ],\n")
-f.write("    cell=[("+str(asestruct.cell[0,0])+", "+str(asestruct.cell[0,1])+", "+str(asestruct.cell[0,2])+"), ("+str(asestruct.cell[1,0])+", "+str(asestruct.cell[1,1])+", "+str(asestruct.cell[1,2])+"), ("+str(asestruct.cell[2,0])+", "+str(asestruct.cell[2,1])+", "+str(asestruct.cell[2,2])+")],\n")
-if Manualpbc == False:
-    f.write("    pbc=True,\n")
-else:
-    f.write("    pbc=["+str(pbcmanual[0])+","+str(pbcmanual[1])+","+str(pbcmanual[2])+"],\n")
-f.write("    )\n")
-f.close()
+    if SolveDoubleElementProblem == True:
+        for n in asestruct.get_chemical_symbols():
+            nn=nn+1
+            for m in positions:
+                mm=mm+1
+                if mm == nn:
+                    f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
+            mm=0
+    else:
+        for n in asestruct.get_chemical_symbols():
+            for m in positions:
+                f.write("    Atom('"+n+"', ( "+str(m[0])+", "+str(m[1])+", "+str(m[2])+" )),\n")
+    f.write("    ],\n")
+    f.write("    cell=[("+str(asestruct.cell[0,0])+", "+str(asestruct.cell[0,1])+", "+str(asestruct.cell[0,2])+"), ("+str(asestruct.cell[1,0])+", "+str(asestruct.cell[1,1])+", "+str(asestruct.cell[1,2])+"), ("+str(asestruct.cell[2,0])+", "+str(asestruct.cell[2,1])+", "+str(asestruct.cell[2,2])+")],\n")
+    if Manualpbc == False:
+        f.write("    pbc=True,\n")
+    else:
+        f.write("    pbc=["+str(pbcmanual[0])+","+str(pbcmanual[1])+","+str(pbcmanual[2])+"],\n")
+    f.write("    )\n")
 
 # PRINT SCREEEN PART -----------------------------------
 print("Result:")
-f = open(os.path.splitext(inFile)[0]+'.py', 'r')
-lines = f.readline()
-while lines:
-    print(lines)
+with open(os.path.splitext(inFile)[0]+'.py', 'r') as f:
     lines = f.readline()
-f.close()
+    while lines:
+        print(lines)
+        lines = f.readline()

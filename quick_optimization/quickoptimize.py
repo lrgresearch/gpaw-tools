332a20321924323e6fdb41fb2ed6401e2448c6c1
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-09-08 17:47:12 +0300

    Update quickoptimize.py

--- quick_optimization/quickoptimize.py
+++ quick_optimization/quickoptimize.py
@@ -1,9 +1,9 @@
 '''
 quickoptimize.py: Quick Geometric Optimization
                   using interatomic potentials.
-Usage: $ python quickoptimize.py ciffilename.cif
+Usage: $ python quickoptimize.py inputfilename.cif
 '''
-import sys
+import sys, os
 from ase import *
 from ase.io import read
 from ase.io.cif import write_cif
@@ -13,11 +13,15 @@ from asap3.md.langevin import Langevin
 from ase.calculators.kim import KIM
 
 
+inFile = sys.argv[1]
+bulk_configuration = read(inFile, index='-1')
+Filename = os.path.splitext(inFile)[0]
+
 # -------------------------------------------------------------
 # Parameters
 # -------------------------------------------------------------
 
-Filename = 'Optimized'
+PotentialUsed = 'LJ_ElliottAkerson_2015_Universal__MO_959249795837_003'
 
 Scaled = False #Scaled or cartessian coordinates
 Manualpbc = False # If you need manual constraint axis
@@ -25,29 +29,12 @@ Manualpbc = False # If you need manual constraint axis
 # IF Manualpbc is true then change following:
 pbcmanual=[True,True,False]
 
-WantCIFexport = True
-
-# -------------------------------------------------------------
-# Bulk Configuration
-# -------------------------------------------------------------
-
-bulk_configuration = Atoms(
-    [
-    Atom('Ge', ( 1.222474e-31, 4.094533468076675e-32, 5.02 )),
-    Atom('Ge', ( -1.9999999993913775e-06, 2.3094022314590417, 4.98 )),
-    ],
-    cell=[(4.0, 0.0, 0.0), (-1.9999999999999991, 3.464101615137755, 0.0), (0.0, 0.0, 20.0)],
-    pbc=True,
-    )
-
-
 # -------------------------------------------------------------
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
 
-bulk_configuration.set_calculator(KIM('LJ_ElliottAkerson_2015_Universal__MO_959249795837_003', options={"ase_neigh": False}))
+bulk_configuration.set_calculator(KIM(PotentialUsed, options={"ase_neigh": False}))
 
-# dyn = VelocityVerlet(bulk_configuration, 5.0 * units.fs, trajectory='md.traj', logfile=Filename+'-Log-GPAW-QuickOptim.txt')
 dyn = Langevin(bulk_configuration, timestep=5*units.fs, trajectory='md.traj', logfile=Filename+'-Log-GPAW-QuickOptim.txt', temperature=1*units.kB, friction=0.05)
 
 
@@ -112,7 +99,8 @@ if Manualpbc == False:
 else:
     print("    pbc=["+str(pbcmanual[0])+","+str(pbcmanual[1])+","+str(pbcmanual[2])+"],")
 print("    )")
-
+print("ATTENTION: If you have double number of atoms, it may be caused by ")
+print("           repeating ASE bug https://gitlab.com/ase/ase/-/issues/169 ")
+print("           Please remove repeating atoms manually with VESTA. Sorry for any inconvenience.")
 # CIF export
-if WantCIFexport == True:
-    write_cif(Filename+'.cif', bulk_configuration)
+write_cif(Filename+'-Optimized.cif', bulk_configuration)

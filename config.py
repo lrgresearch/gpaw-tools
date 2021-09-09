dca38967a6df5359c6bf7c97a20841b40bed607a
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-09-09 17:11:51 +0300

    Update config.py
    It may be better to give filled config.py

--- config.py
+++ config.py
@@ -1,1 +1,25 @@
-
+Use_PW = True
+DOS_calc = True
+Band_calc = True
+Density_calc = False
+Optical_calc = False
+fmaxval = 0.05
+cut_off_energy = 340
+kpts_x = 3
+kpts_y = 3
+kpts_z = 3
+band_path = 'GXLG'
+band_npoints = 40
+energy_max = 10
+XC_calc = 'LDA'
+Spin_calc = True
+gridref = 4
+num_of_bands = 16
+optFDsmear = 0.05
+opteta = 0.05
+optdomega0 = 0.02
+optnblocks = 4
+draw_graphs = True
+whichstrain = [False, False, False, False, False, False]
+WantCIFexport = True
+MPIcores = 2

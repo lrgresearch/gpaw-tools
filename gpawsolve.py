88962a6a7a8e4c9f33ff1349ac1c6c789d436004
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-10-01 14:18:16 +0300

    New minor release

--- gpawsolve.py
+++ gpawsolve.py
@@ -135,7 +135,7 @@ bulk_configuration = Atoms(
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
 # Version
-__version__ = "v21.10.1b1"
+__version__ = "v21.10.1"
 
 # To print Description variable with argparse
 class RawFormatter(HelpFormatter):

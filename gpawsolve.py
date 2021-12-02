0eeab8b5744fd89efc4e7978264f8ce1597da166
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-12-02 22:08:56 +0300

    Change version

--- gpawsolve.py
+++ gpawsolve.py
@@ -134,7 +134,7 @@ bulk_configuration = Atoms(
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
 # Version
-__version__ = "v21.12.0"
+__version__ = "v21.12.1b1"
 
 # To print Description variable with argparse
 class RawFormatter(HelpFormatter):

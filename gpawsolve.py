663cf579e125f601f3baeb08801f8c9e6b6286be
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2021-12-08 17:04:34 +0300

    Saving in both gpw and json file

--- gpawsolve.py
+++ gpawsolve.py
@@ -471,7 +471,7 @@ if Optical_calc == False:
             num_of_bands = calc.get_number_of_bands()
             parprint('Num of bands:'+str(num_of_bands))
 
-            #bs.write(struct+'-3-Result-Band.json')
+            calc.write(struct+'-3-Result-Band.json')
             calc.write(struct+'-3-Result-Band.gpw')
 
             if Spin_calc == True:

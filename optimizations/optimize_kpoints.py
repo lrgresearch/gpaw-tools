11791160a1ef6225943a0756292b6da5218da7e9
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2022-04-05 17:39:32 +0300

    Update optimize_kpoints.py

--- optimizations/optimize_kpoints.py
+++ optimizations/optimize_kpoints.py
@@ -24,14 +24,14 @@ kpoint_max = 11
 # -------------------------------------------------------------
 # Bulk Configuration - YOU DON'T NEED IF YOU USE CIF FILE
 # -------------------------------------------------------------
-bulk_configuration = Atoms(
-    [
-    Atom('Si', ( 0.0, 0.0, 0.0 )),
-    Atom('Si', ( 1.35765, 1.35765, 1.35765 )),
-    ],
-    cell=[(0.0, 2.7153, 2.7153), (2.7153, 0.0, 2.7153), (2.7153, 2.7153, 0.0)],
-    pbc=True,
-    )
+bulk_configuration = Atoms(['C' for i in range(2)],
+              [(   3.790492,    2.188441,    1.547462),
+               (   2.526995,    1.458961,    1.031641)],
+              pbc = (True,True,True))
+bulk_configuration.set_cell([[    2.526995,     0.000000,     0.000000],
+                [    1.263497,     2.188441,     0.000000],
+                [    1.263497,     0.729480,     2.063282]],
+                scale_atoms = False)
 # -------------------------------------------------------------
 # CONVERGE KPOINTS
 # -------------------------------------------------------------
@@ -45,7 +45,7 @@ with paropen('OptimizeKPoints_Table-KPoints.txt', 'a') as f:
     f.write('K-point  Total_Energy\n')
     for k in range(kpoint_min, kpoint_max):
         bulk_configuration.calc = GPAW(mode=PW(cutoffenergy),
-                                    xc='PBE',
+                                    xc='GLLBSCM',
                                     kpts=(k, k, k),
                                     parallel={'band': 1},
                                     basis='dzp',

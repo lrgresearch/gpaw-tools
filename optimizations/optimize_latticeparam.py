3e690d9d28b3c61a346a90701791f96a314962bb
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2022-04-05 17:45:55 +0300

    optimize_latticeparam.py can work with kptsdensity

--- optimizations/optimize_latticeparam.py
+++ optimizations/optimize_latticeparam.py
@@ -13,13 +13,19 @@ from ase.parallel import paropen, world, parprint
 # -------------------------------------------------------------
 # ENTER PARAMETERS
 # -------------------------------------------------------------
-cutoff = 400
+cutoff = 700
 
-kpts_x = 3
-kpts_y = 3
-kpts_z = 1
+kpts_x = 5
+kpts_y = 5
+kpts_z = 5
 
-a0 = 3.7 # Creating a lattice parameter list
+xc_used = 'GLLBSCM'
+
+kpts_density =2.5
+
+use_density = True # Change to "True", if you want to use k-point density instead of kpoints.
+
+a0 = 2.53 # Creating a lattice parameter list
 percent = 0.05 # changing the lattice parameter how much percent?
 
 a_list = a0 * (1 + np.linspace(-percent , +percent , 11)) # do not change this line
@@ -46,23 +52,32 @@ with paropen('Optimize-Lattice_Table-LatticeParam.txt', 'w') as f:
         # -------------------------------------------------------------
         # DO NOT FORGET TO INSERT BULK CONFIGURATION UNDER HERE
         # WITH PROPER INDENTATION!!!
-        # -------------------------------------------------------------
-        bulk_configuration = Atoms(
-            [
-            Atom('Sn', ( 0.0, 0.0, 5.02 )),
-            Atom('C', ( 0.0, 2.3094022314590417*latt_a/4.0, 4.98 )),
-            ],
-            cell=[(4.0, 0.0, 0.0), (-1.9999999999999991*latt_a/4.0, 3.464101615137755*latt_a/4.0, 0.0), (0.0, 0.0, 20.0)],
-            pbc=True,
-            )
+        # -------------------------------------------------------------           
+        bulk_configuration = Atoms(['C' for i in range(2)],
+              [(   3.790492*latt_a/2.526995,    2.188441*latt_a/2.526995,    1.547462*latt_a/2.526995),
+               (   2.526995*latt_a/2.526995,    1.458961*latt_a/2.526995,    1.031641*latt_a/2.526995)],
+              pbc = (True,True,True))
+        bulk_configuration.set_cell([[    2.526995*latt_a/2.526995,     0.000000,     0.000000],
+                [    1.263497*latt_a/2.526995,     2.188441*latt_a/2.526995,     0.000000],
+                [    1.263497*latt_a/2.526995,     0.729480*latt_a/2.526995,     2.063282*latt_a/2.526995]],
+                scale_atoms = False)
+
         # --------------------------------------------------------------
         # create the cell, then find, store and print the total energy for a in a_list:
-        bulk_configuration.calc = GPAW(mode=PW(cutoff),
-                                    parallel={'domain': world.size},
-                                    xc='PBE',
-                                    kpts=(kpts_x, kpts_y, kpts_z),
-                                    basis='dzp',
-                                    txt='Optimize-Lattice_%.2f.txt' % latt_a)
+        if use_density == True:
+            bulk_configuration.calc = GPAW(mode=PW(cutoff),
+                                        parallel={'domain': world.size},
+                                        xc=xc_used,
+                                        kpts={'density': kpts_density, 'gamma': True},
+                                        basis='dzp',
+                                        txt='Optimize-Lattice_%.2f.txt' % latt_a)
+        else:
+            bulk_configuration.calc = GPAW(mode=PW(cutoff),
+                                        parallel={'domain': world.size},
+                                        xc=xc_used,
+                                        kpts=(kpts_x, kpts_y, kpts_z),
+                                        basis='dzp',
+                                        txt='Optimize-Lattice_%.2f.txt' % latt_a)
         # we need volume and energy for E(V)−curve;
         # use corresponding getter functions and append values to lists 
         vol = bulk_configuration.get_volume()

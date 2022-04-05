f8782e4545fbc82ab82a66230e0e6dc9867ae245
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2022-04-05 17:36:43 +0300

    Create optimize_kptsdensity.py

---
+++ optimizations/optimize_kptsdensity.py
@@ -0,0 +1,46 @@
+'''
+optimize_kptsdensity.py: Sample K-point density optimization with GPAW
+
+Usage: $ gpaw -P<core_number> python optimize_kptsdensity.py
+'''
+
+from ase import *
+from gpaw import GPAW, PW
+from ase.parallel import paropen, parprint
+import numpy as np
+
+# -------------------------------------------------------------
+# ENTER PARAMETERS
+# -------------------------------------------------------------
+cutoffenergy = 700
+kptsdensity_min = 1.5
+kptsdensity_max = 3.0
+kptsdensity_step = 0.1
+
+
+# -------------------------------------------------------------
+# Bulk Configuration
+# -------------------------------------------------------------
+bulk_configuration = Atoms(['C' for i in range(2)],
+              [(   3.790492,    2.188441,    1.547462),
+               (   2.526995,    1.458961,    1.031641)],
+              pbc = (True,True,True))
+bulk_configuration.set_cell([[    2.526995,     0.000000,     0.000000],
+                [    1.263497,     2.188441,     0.000000],
+                [    1.263497,     0.729480,     2.063282]],
+                scale_atoms = False)
+# -------------------------------------------------------------
+# CONVERGE KPOINTS
+# -------------------------------------------------------------
+cell0 = bulk_configuration.cell
+with paropen('OptimizeKPoints_Table-KPointDensity.txt', 'a') as f:
+    f.write('K-density  Total_Energy\n')
+    for k in np.arange(kptsdensity_min, kptsdensity_max, kptsdensity_step):
+        bulk_configuration.calc = GPAW(mode=PW(cutoffenergy),
+                                    xc='GLLBSCM',
+                                    kpts={'density': k, 'gamma': True},
+                                    parallel={'band': 1},
+                                    basis='dzp',
+                                    txt='OptimizeKPointDensity_KDensity-%02d.txt' % k)
+        parprint("K-density:"+str(k)+"   Potential_Energy:"+str(bulk_configuration.get_potential_energy()))
+        f.write(str(k)+'  '+str(bulk_configuration.get_potential_energy())+'\n')

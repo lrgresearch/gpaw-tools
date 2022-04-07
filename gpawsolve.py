3a1e203d95a2c4ded9e79031fd8e2a24ea596f20
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2022-04-07 09:35:01 +0300

    Increasing the version number

--- gpawsolve.py
+++ gpawsolve.py
@@ -149,7 +149,7 @@ bulk_configuration = Atoms(
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
 # Version
-__version__ = "v22.3.1b1"
+__version__ = "v22.4.0"
 
 # To print Description variable with argparse
 class RawFormatter(HelpFormatter):

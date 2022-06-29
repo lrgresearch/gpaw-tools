972f271f01132ef8d69ffcdc367b88031896a112
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2022-06-29 13:08:57 +0300

    quickoptimize.py work like gpawsolve.py

--- quick_optimization/quickoptimize.py
+++ quick_optimization/quickoptimize.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python
+
 '''
 quickoptimize.py: Quick Geometric Optimization
                   using interatomic potentials.
-Usage: $ python quickoptimize.py geometryfile.cif
+More information: $ quickoptimize.py -h
 '''
-import sys, os
+
+Description = f''' 
+ Usage: 
+ $ python quickoptimize.py <args>
+ 
+ -------------------------------------------------------------
+   Some potentials
+ -------------------------------------------------------------
+ | Name                                                  | Information                           | 
+ | ----------------------------------------------------- | ------------------------------------- |
+ | LJ_ElliottAkerson_2015_Universal__MO_959249795837_003 | General potential for all elements    |
+ 
+ '''
+
+import getopt, sys, os, time
+import spglib as spg
+import textwrap
+import requests
+from argparse import ArgumentParser, HelpFormatter
 from pathlib import Path
 from ase import *
 from ase.io import read
@@ -19,7 +39,7 @@ from ase.calculators.kim import KIM
 
 # Simulation parameters
 
-#More potantial can be found from https://openkim.org
+#More potential can be found from https://openkim.org
 PotentialUsed = 'LJ_ElliottAkerson_2015_Universal__MO_959249795837_003'
 Temperature = 1 #Kelvin
 Time = 5 # fs
@@ -53,26 +73,99 @@ bulk_configuration = Atoms(
 # -------------------------------------------------------------
 # ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
 # -------------------------------------------------------------
-asestruct = bulk_configuration
 
-# Basic argument triage
-if len(sys.argv) == 1:
-    Filename = 'Optimized'
-    print("Number of atoms provided in Atoms object:"+str(asestruct.get_global_number_of_atoms()))
-elif len(sys.argv) == 2:
-    inFile = sys.argv[1]
-    Filename = os.path.splitext(inFile)[0]
-    struct = Path(inFile).stem
-    asestruct = read(inFile, index='-1')
-    print("Number of atoms imported from CIF file:"+str(asestruct.get_global_number_of_atoms()))
+# Start time
+time0 = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+# To print Description variable with argparse
+class RawFormatter(HelpFormatter):
+    def _fill_text(self, text, width, indent):
+        return "\n".join([textwrap.fill(line, width) for line in textwrap.indent(textwrap.dedent(text), indent).splitlines()])
+
+# Arguments parsing
+parser = ArgumentParser(prog ='quickoptimize.py', description=Description, formatter_class=RawFormatter)
+
+
+parser.add_argument("-i", "--input", dest = "inputfile", help="Use input file for calculation variables (also you can insert geometry)")
+parser.add_argument("-g", "--geometry",dest ="geometryfile", help="Use CIF file for geometry")
+parser.add_argument("-v", "--version", dest="version", action='store_true')
+args = None
+
+args = parser.parse_args()
+
+if args is None:
+    print(" Please enter input file (-i) and geometryfile (-g) to execute.")
+    exit(0)
+
+outdir = True
+inFile = None
+Outdirname = ''
+
+try:
+    if args.inputfile is not None:
+        configpath = os.path.join(os.getcwd(),args.inputfile)
+        sys.path.append(os.getcwd())
+        # Works like from FILE import *
+        conf = __import__(Path(configpath).stem, globals(), locals(), ['*'])
+        for k in dir(conf):
+            locals()[k] = getattr(conf, k)
+
+    if args.geometryfile :
+        inFile = os.path.join(os.getcwd(),args.geometryfile)
+    
+    if args.version == True:
+        import asap3
+        import ase
+        try:
+            response = requests.get("https://api.github.com/repos/lrgresearch/gpaw-tools/releases/latest", timeout=5)
+            print('-------------------------------------------------------------------------------------------------------')
+            print('\033[95mgpaw-tools:\033[0m This is quickoptimize.py uses ASAP3 '+asap3.__version__+', and ASE '+ase.__version__)
+            print('-------------------------------------------------------------------------------------------------------')
+            print('The latest STABLE release was '+response.json()["tag_name"]+', which is published at '+response.json()["published_at"])
+            print('Download the latest STABLE tarball release at: '+response.json()["tarball_url"])
+            print('Download the latest STABLE zipball release at: '+response.json()["zipball_url"])
+            print('Download the latest DEV zipball release at: https://github.com/lrgresearch/gpaw-tools/archive/refs/heads/main.zip')
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            print('-------------------------------------------------------------------------------------------------------')
+            print('gpaw-tools: This is quickoptimize.py uses ASAP3 '+asap3.__version__+', ASE '+ase.__version__)
+            print('-------------------------------------------------------------------------------------------------------')
+            print('No internet connection available.')
+        quit()      
+
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+
+# If there is a CIF input, use it. Otherwise use the bulk configuration provided above.
+if inFile is None:
+    if Outdirname !='':
+        struct = Outdirname
+    else:
+        struct = 'results' # All files will get their names from this file
+    print("Number of atoms provided in Atoms object:"+str(bulk_configuration.get_global_number_of_atoms()))
 else:
-    print("Wrong number of argument. Usage: python quickoptimize.py inputfile.cif")
-    quit()
+    struct = Path(inFile).stem
+    bulk_configuration = read(inFile, index='-1')
+    print("Number of atoms imported from CIF file:"+str(bulk_configuration.get_global_number_of_atoms()))
+    print("Spacegroup of CIF file (SPGlib):",spg.get_spacegroup(bulk_configuration))
+
+# Control outdir
+if Outdirname != '':
+    structpath = os.path.join(os.getcwd(),Outdirname)
+else:
+    structpath = os.path.join(os.getcwd(),struct)
+
+if not os.path.isdir(structpath):
+    os.makedirs(structpath, exist_ok=True)
+struct = os.path.join(structpath,struct)
+
+# -------------------------------------------------------------
+# ///////   YOU DO NOT NEED TO CHANGE ANYTHING BELOW    \\\\\\\
+# -------------------------------------------------------------
+asestruct = bulk_configuration
 
 asestruct.set_calculator(KIM(PotentialUsed, options={"ase_neigh": False}))
 
-dyn = Langevin(asestruct, timestep=Time*units.fs, trajectory='quickoptim.traj', logfile=Filename+'-Log-GPAW-QuickOptim.txt', temperature_K=Temperature, friction=Friction)
-
+dyn = Langevin(asestruct, timestep=Time*units.fs, trajectory=struct+'-Results.traj', logfile=struct+'-Log.txt', temperature_K=Temperature, friction=Friction)
 
 print("")
 print("Energy per atom:")
@@ -85,7 +178,7 @@ for i in range(25):
     print("%15.5f %15.5f %15.5f" % (epot, ekin, epot+ekin))
 
 # PRINT TO FILE PART -----------------------------------
-with open(Filename+'.py', 'w') as f:
+with open(struct+'Results.py', 'w') as f:
     f.write("bulk_configuration = Atoms(\n")
     f.write("    [\n")
     if Scaled == True:
@@ -117,13 +210,11 @@ with open(Filename+'.py', 'w') as f:
 
 # PRINT SCREEEN PART -----------------------------------
 print("Result:")
-with open(Filename+'.py', 'r') as f:
+with open(struct+'Results.py', 'r') as f:
     lines = f.readline()
     while lines:
         print(lines)
         lines = f.readline()
-
-
 
 print("    )")
 print("ATTENTION: If you have double number of atoms, it may be caused by ")
@@ -132,4 +223,4 @@ print("           Please assign SolveDoubleElementProblem variable as True in th
 
 
 # CIF export
-write_cif(Filename+'-Optimized.cif', bulk_configuration)
+write_cif(struct+'-Optimized.cif', bulk_configuration)
---
+++ quick_optimization/sampleinput.py
@@ -0,0 +1,15 @@
+PotentialUsed = 'LJ_ElliottAkerson_2015_Universal__MO_959249795837_003'
+Temperature = 1 #Kelvin
+Time = 5 # fs
+Friction = 0.05
+
+Scaled = False #Scaled or cartessian coordinates
+Manualpbc = False # If you need manual constraint axis
+
+# IF Manualpbc is true then change following:
+pbcmanual=[True,True,False]
+
+# If you have double number of elements in your final file
+SolveDoubleElementProblem = True
+
+

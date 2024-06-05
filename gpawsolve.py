f9ea4af47b008d801b65b1f6f84f79b235370356
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2024-06-05 10:49:38 +0300

    Increasing version to 24.6.0

--- gpawsolve.py
+++ gpawsolve.py
@@ -1780,7 +1780,7 @@ if __name__ == "__main__":
     # ------------------ End of Localisation Tables --------------------------
     
     # Version
-    __version__ = "v23.10.1b1"
+    __version__ = "v24.6.0"
 
     parser = ArgumentParser(prog ='gpawtools.py', description=Description, formatter_class=RawFormatter)
     parser.add_argument("-i", "--input", dest = "inputfile", help="Use input file for calculation variables (also you can insert geometry)")
